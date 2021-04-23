54ca5d27973349fc6de67037ec3717cc8c218d6e
rain_chat <super.pasha4444@yandex.ru>
2021-04-23 14:53:20 +1000

    1.1

--- src/main/java/com/rainchat/rainlib/RainCore.java
+++ src/main/java/com/rainchat/rainlib/RainCore.java
@@ -1,20 +1,4 @@
 package com.rainchat.rainlib;
 
-import org.bukkit.plugin.java.JavaPlugin;
 
-
-public final class RainCore extends JavaPlugin {
-
-
-    @Override
-    public void onEnable() {
-
-    }
-
-    @Override
-    public void onDisable() {
-
-    }
-
-
-}
+public final class RainCore {}
